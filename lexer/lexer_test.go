1329673daa6aaf58a792447e25a4a09bc1a19ac7
Chamoda Pandithage <chamoda@xaventra.com>
2025-07-27 12:29:49 +0530

    End of chapter 2

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -11,18 +11,17 @@ func TestNextToken(t *testing.T) {
 let ten = 10;
 
 let add = fn(x, y) {
-x + y;
+  x + y;
 };
 
 let result = add(five, ten);
-
 !-/*5;
 5 < 10 > 5;
 
 if (5 < 10) {
-return true;
+	return true;
 } else {
-return false;
+	return false;
 }
 
 10 == 10;
@@ -113,10 +112,12 @@ return false;
 
 	for i, tt := range tests {
 		tok := l.NextToken()
+
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
 		}
+
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
 				i, tt.expectedLiteral, tok.Literal)
